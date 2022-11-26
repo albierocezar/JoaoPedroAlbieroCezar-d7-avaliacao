c89a006bc04676a1bc88cd40e7e44642e2e78613
albierocezar <albierocezar@usp.br>
2022-11-26 10:13:21 -0300

    Layout complete

--- JoaoPedroAlbieroCezar-d7-avaliacao/JoaoPedroAlbieroCezar-d7-avaliacao/MainWindow.xaml.cs
+++ JoaoPedroAlbieroCezar-d7-avaliacao/JoaoPedroAlbieroCezar-d7-avaliacao/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace JoaoPedroAlbieroCezar_d7_avaliacao
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
